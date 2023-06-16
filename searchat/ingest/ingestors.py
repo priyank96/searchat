1ec33505e6629db75021124b6715ad469fe297a1
priyank96 <priyankbhandia@gmail.com>
2023-06-16 19:38:17 +0530

    added chat api

--- searchat/ingest/ingestors.py
+++ searchat/ingest/ingestors.py
@@ -1,22 +1,19 @@
 from typing import List
 
+import html2text
 import requests
-from bs4 import BeautifulSoup
-from langchain.text_splitter import CharacterTextSplitter
 from data import store_manager
+from langchain.text_splitter import CharacterTextSplitter
 
 
 class WebIngestor:
 
     @staticmethod
-    def _extract_webpage_text(url, chunk_size: int = 500, separator: str = '\n'):
+    def _extract_webpage_text(url, chunk_size: int = 1000, separator: str = '\n'):
         text_splitter = CharacterTextSplitter(chunk_size=chunk_size, separator=separator)
         html = requests.get(url).text
-        soup = BeautifulSoup(html, features="html.parser")
-        page_text = soup.get_text()
-        lines = (line.strip() for line in page_text.splitlines())
-        text = '\n'.join(line for line in lines if line)
-        return text_splitter.split_text(text)
+        page_text = html2text.HTML2Text(html)
+        return text_splitter.split_text(page_text)
 
     @staticmethod
     def _ingest_text(text: List[str]):
