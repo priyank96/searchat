c5df5acb5be7ca150efcad8db8387676b32ba0b8
priyank96 <priyankbhandia@gmail.com>
2023-06-16 19:02:04 +0530

    added chat api

--- searchat/ingest/ingestors.py
+++ searchat/ingest/ingestors.py
@@ -9,13 +9,13 @@ from data import store_manager
 class WebIngestor:
 
     @staticmethod
-    def _extract_webpage_text(url, chunk_size: int = 1500, separator: str = '\n'):
+    def _extract_webpage_text(url, chunk_size: int = 500, separator: str = '\n'):
         text_splitter = CharacterTextSplitter(chunk_size=chunk_size, separator=separator)
         html = requests.get(url).text
         soup = BeautifulSoup(html, features="html.parser")
         page_text = soup.get_text()
         lines = (line.strip() for line in page_text.splitlines())
-        text = '\n\n'.join(line for line in lines if line)
+        text = '\n'.join(line for line in lines if line)
         return text_splitter.split_text(text)
 
     @staticmethod
